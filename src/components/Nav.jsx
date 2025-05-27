f9f45ce3c6c48f10e75f989d2bf321c61a15fe21
kottor <gardien243@gmail.com>
2025-05-27 11:16:48 +0200

    nav components fait

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,19 @@
-import '../assets/img/'
+import { Link } from 'react-router-dom'
+import logo from '../assets/shared/logo.svg'
 
 
 export default function NavBar(){
     return(
         <nav>
-
+            <div>
+                <img src={logo} alt="logo"/>
+            </div>
+            <div>
+                <Link to={'/'}>01 HOME</Link>
+                <Link to={'/Destination'}>02 DESTINATION</Link>
+                <Link to={'/Crew'}>03 CREW</Link>
+                <Link to={'/Tech'}>04 TECHONOLOGY</Link>
+            </div>
         </nav>
     )
 }
